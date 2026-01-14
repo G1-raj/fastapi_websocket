15bfc981dc6f7408b6281740d9fb05fde04cbd30
G1-raj <gullurajput18@gmail.com>
2026-01-14 22:54:15 +0530

    added model to signup user

---
+++ models/models.py
@@ -0,0 +1,14 @@
+from sqlalchemy import Boolean, Column, DateTime, ForeignKey, Float, Integer, String
+from sqlalchemy.orm import relationship
+from datetime import datetime, timezone
+from sqlalchemy.orm import declarative_base
+
+Base = declarative_base
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True)
+    full_name = Column(String, nullable=False)
+    email = Column(String, unique=True, nullable=False)
+    hashed_password = Column(String, nullable=False)
